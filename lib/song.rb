42716994303b76c793d44053c5ebb747d3a25913
Collin Jilbert <cjilbert504@gmail.com>
2019-12-06 15:22:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,23 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+      song = self.new
+      song.name = name
+      @@all << song
+      song
+    end
+
+    def self.new_by_name(title)
+      song = self.new
+      song.name = title
+      song
+    end
+
+    def self.create_by_name(title)
+      song = self.new
+      song.name = title
+      @@all << song
+      song
+    end
 end
