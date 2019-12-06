40d45ea20a6e532cc66247e0eff18e7ba67be606
Collin Jilbert <cjilbert504@gmail.com>
2019-12-06 15:36:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,12 +43,12 @@ class Song
   end
 
   def self.new_from_filename(name)
-      song = self.new 
+      song = self.new
       song.name = (name.split(" - ")[1].chomp(".mp3"))
       song.artist_name = (name.split(" - ")[0])
       song
     end
-    
+
     def self.create_from_filename(name)
       song = self.new
       song.name = (name.split(" - ")[1].chomp(".mp3"))
@@ -56,7 +56,7 @@ class Song
       @@all << song
       song
     end
-    
+
     def self.destroy_all()
       @@all.clear
     end
